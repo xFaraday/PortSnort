548ed1e698987ecb29b763d8130a69bb8bebcb3a
xfaraday <theonlyfaraday@gmail.com>
2020-10-18 02:07:32 -0700

    added support for ip ranges

--- portsnort.sh
+++ portsnort.sh
@@ -10,8 +10,6 @@ port_state="--open-only"
 #nmap options, set defaults
 opt="-A"
 
-
-
 #running checks
 if ! [ -x "$(command -v masscan)" ]; then
   echo 'Error: Masscan is not installed.' >&2
@@ -28,12 +26,11 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-
 #getting options, r=rate i=interface t=target_ip n=nmap arguments h=help
 while getopts 'r:i:t:n:h' option; do
   case "$option" in
     r)rate=${OPTARG};;
-	p)ports=${OPTARG};;
+    p)ports=${OPTARG};;
     i)interface=${OPTARG};;
     t)target_ip=${OPTARG};;
     n)opt=${OPTARG};;
@@ -42,26 +39,51 @@ while getopts 'r:i:t:n:h' option; do
 done
 
 usage() {
+printf -- "\n\n<Questions to the dev>\n\n"
+printf -- "User: Whats loud network traffic?\n\n"
+printf -- "Dev: No idea\n\n"
+			
 printf -- "-r = masscan scan rate. \n"
 printf -- "-p = port range to scan.  Default is 1-65535\n"
 printf -- "-i = Network interface to scan from\n"
-printf -- "-t = target ip to scan.  Currently only supports 1 IP at a time\n"
+printf -- "-t = target ip to scan.  Supports IP ranges\n"
 printf -- "-n = nmap options, Default is -A\n"
 printf -- "-h = help\n"
 }
 
-if ! [ -z "$target_ip" ]; then 
-  masscan --rate "$rate" -p "$ports" --adapter "$interface" "$target_ip" "$port_state" > masscan.txt
-  wait
-  nmap_scan_ports=$(sed 's/[ \t]*\([0-9]\{1,\}\).*/\1/' masscan.txt | cut -c 21- | awk '{print}' ORS=',')
-    if ! [ -z "$nmap_scan_ports" ]; then
-      nmap -A -p "${nmap_scan_ports::-1}" "$target_ip" > initial_scan	
-    else
-      echo "no open tcp ports, aborting scan"
-      exit 1
-    fi
+main() {
+if ! [ -z "$target_ip" ]; then
+	rangecheck=$(echo "$target_ip" | grep "/")
+	if ! [ -z "$rangecheck" ]; then
+		masscan --rate "$rate" -p "$ports" --adapter "$interface" "$target_ip" "$port_state" > masscan.txt
+		wait
+		ipalive=$(awk '{ print $6 }' masscan.txt | uniq) 
+		numuniq=$(awk '{ print $6 }' masscan.txt | uniq | wc -l)
+		for ip in $ipalive; do
+			nmap_scan_ports=$(grep $ip masscan.txt | sed 's/[ \t]*\([0-9]\{1,\}\).*/\1/' masscan.txt | cut -c 21- | awk '{print}' ORS=',')
+			if ! [ -z "$nmap_scan_ports" ]; then
+      			nmap -A -p "${nmap_scan_ports::-1}" "$target_ip" > "$ip"	
+    		else
+      			echo "no open tcp ports, aborting scan"
+      			exit 1
+    		fi
+		done
+	else
+		masscan --rate "$rate" -p "$ports" --adapter "$interface" "$target_ip" "$port_state" > masscan.txt
+		wait
+		nmap_scan_ports=$(sed 's/[ \t]*\([0-9]\{1,\}\).*/\1/' masscan.txt | cut -c 21- | awk '{print}' ORS=',')
+		if ! [ -z "$nmap_scan_ports" ]; then
+      		nmap -p "${nmap_scan_ports::-1}" "$target_ip" > "$target_ip"	
+    	else
+      		echo "no open tcp ports, aborting scan"
+      		exit 1
+    	fi
+	fi
 else
-  printf "\n No target IP specified.  Refer to usage.\n\n"
-  usage
-  exit 1
+    printf "\n No target IP specified. Refer to usage.\n\n"
+    usage
+    exit 1
 fi
+}
+
+main "target_ip" "$ports" "$interface" "$port_state"
